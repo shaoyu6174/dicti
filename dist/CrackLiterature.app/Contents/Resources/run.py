5d67a4bc7d8bad862bab296879d8a8b941ebbed0
Exitialium <henrywu0424@gmail.com>
2020-05-02 19:17:24 +0800

    distributable files

---
+++ dist/CrackLiterature.app/Contents/Resources/run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import utils
+import os
+
+
+if os.path.exists("result.json"):
+    os.remove("result.json")
+with open("input.txt", 'r') as f:
+    contents = f.readlines()
+    wordlist = []
+    for line in contents:
+        wordlist.append(line.rstrip())
+    words = ",".join(wordlist)
+
+utils.crawl(words)
+utils.process()
