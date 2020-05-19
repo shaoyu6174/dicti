07ddcc9c5ccf40e2342e6755adec43aef73c0188
Shaoyu Tseng <shaoyu0209@tuta.io>
2020-05-19 12:21:31 +0800

    fix broken file

---
+++ dicti/spiders/beta.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+class worddef(scrapy.Item):
+    word = scrapy.Field()
+    defi = scrapy.Field()
+
+class BetaSpider(scrapy.Spider):
+    name = 'beta'
+    allowed_domains = ['dictionary.com']
+
+    def __init__(self, words):
+        self.start_urls = ["http://dictionary.com/browse/" + w for w in words.split(",")]
+        print(self.start_urls)
+
+    def parse(self, response):
+        item = worddef()
+        item["word"] = response.css("h1::text").get()
+        item["defi"] = response.css("div.e16867sm0:first-of-type span.e1q3nk1v4").getall()
+        yield item
